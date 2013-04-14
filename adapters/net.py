d18f2c1f0dd7b7ea1f75f4d9bb73cf1cd6697a11
Kristofer Hallin <kristofer.hallin@gmail.com>
2013-04-14 21:08:35 +0200

    Class for getting network interface card statistics

--- adapters/net.py
+++ adapters/net.py
@@ -1,2 +1,27 @@
-def main():
-    print "net"
+import os
+
+class Net():
+    """ Class to get network interface statistics. """
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return ""
+
+    def __get_stats__(self, dev):
+        stats = []
+        for stat in ['rx_bytes', 'tx_bytes']:
+            with open('/sys/class/net/' + dev + '/statistics/' + stat) as fd:
+                dev_stat = fd.read()
+                fd.close()
+            stats.append(dev_stat.rstrip('\n'))
+        return stats
+
+    def get_stats(self, dev):
+        return self.__get_stats__(dev)
+
+if __name__ == '__main__':
+    n = Net()
+    print n
+    print n.__get_stats__('wlan0')
