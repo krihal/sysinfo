2a521c748e6fec0c922f2bbac77ea524a22f003b
Kristofer Hallin <kristofer.hallin@gmail.com>
2013-04-15 21:11:02 +0200

    Removed test adapter

--- config.py
+++ config.py
@@ -5,6 +5,3 @@ adapters = dict()
 adapters['net'] = {"net": "net", 
                  "file": "net",
                  "interval": 5}
-adapters['test'] = {"name": "test",
-                    "file": "test",
-                    "interval": 1}
