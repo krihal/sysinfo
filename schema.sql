c4bdf3b33a69cc9251f0129054924e230fe6aab3
Kristofer Hallin <kristofer.hallin@gmail.com>
2013-04-15 21:08:42 +0200

    Changed SQL schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,6 @@
 drop table if exists sys_stat;
 create table sys_stat (
-       id integer primary key autoincrement not null,
        time timestamp default (strftime('%s', 'now')) not null,
-       cpu float,
-       mem float,
-       net float,
-       users integer,
-       load_one float,
-       load_five float,
-       load_fifteen float
+       label text not null,
+       value float not null
 );     
