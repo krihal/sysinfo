9710b06a0318e368b2bab72c64324d3e0e579e39
Kristofer Hallin <khallin@accedian.com>
2013-04-16 13:44:32 +0200

    Added tables for aggregated values

--- schema.sql
+++ schema.sql
@@ -4,3 +4,31 @@ create table sys_stat (
        label text not null,
        value float not null
 );     
+
+drop table if exists sys_aggregated_hourly;
+create table sys_aggregated_hourly {
+       time timestamp default (strftime('%s', 'now')) not null,
+       label text not null,
+       value float not null
+};
+
+drop table if exists sys_aggregated_daily;
+create table sys_aggregated_daily {
+       time timestamp default (strftime('%s', 'now')) not null,
+       label text not null,
+       value float not null
+};
+
+drop table if exists sys_aggregated_weekly;
+create table sys_aggregated_weekly {
+       time timestamp default (strftime('%s', 'now')) not null,
+       label text not null,
+       value float not null
+};
+
+drop table if exists sys_aggregated_monthly;
+create table sys_aggregated_monthly {
+       time timestamp default (strftime('%s', 'now')) not null,
+       label text not null,
+       value float not null
+};
