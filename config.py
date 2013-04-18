2984ba91a94a3dba4980cca954aabbba0bc3b32a
Kristofer Hallin <kristofer.hallin@gmail.com>
2013-04-18 21:53:11 +0200

    Added memory adapter

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 adapters = dict()
 
-#adapters[cpu] = {"name": "cpu", 
-#                 "file": "cpu"}
-adapters['net'] = {"net": "net", 
-                 "file": "net",
-                 "interval": 1}
+adapters['cpu'] = {"name": "cpu", "file": "cpu", 'interval': 2}
+adapters['net'] = {"net": "net", "file": "net", "interval": 1}
+adapters['mem'] = {"mem": "mem", "file": "mem", "interval": 10}
